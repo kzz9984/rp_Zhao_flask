4c97f5ff748a54508436e4f007fc93668ea0c83d
kzz9984 <92399816+kzz9984@users.noreply.github.com>
2023-11-12 19:23:02 -0500

    fixed repo name in link

--- js/lineChart.js
+++ js/lineChart.js
@@ -3,7 +3,7 @@
 */
 
 async function getData() {
-    const response = await fetch('/zhaoSE2324/data/trial-times.csv');
+    const response = await fetch('/rp_Zhao/data/trial-times.csv');
     //const response = await fetch('../data/trial-times.csv');
     const data = await response.text();     // CSV is in TEXT format
 
